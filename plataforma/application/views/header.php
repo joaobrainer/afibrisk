ff3d4be7a5b8494a445abb050946cc8f6bcffe4f
SrtaVerdant <gabriela.mrezende@outlook.com.br>
2024-03-25 22:28:11 -0300

    em andamento

--- plataforma/application/views/header.php
+++ plataforma/application/views/header.php
@@ -10,7 +10,7 @@
 
 <!-- <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"> -->
 <!-- <link rel="icon" href="/favicon.ico" type="image/x-icon"> -->
-
+<link href="https://fonts.cdnfonts.com/css/multicolore-pro" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
